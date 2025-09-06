76f3f91bba0bae29ef116ae0964cca8fb23d69e2
Goverdhan Gaur <goverdhangaur02@gmail.com>
2025-09-06 14:37:29 -0400

    WA-30 Added Basic List UI for Templates

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -1,9 +1,35 @@
 
 export default function History() {
   return (
-    <div className="text-2xl gap-4 container mx-auto">
-
-      Hello World!! This is a workoasdast appsss
+    <div className="gap-4 container mx-auto">
+      <h2 className="text-3xl font-bold mb-3">Templates</h2>
+      <div data-table="templates">
+        <div data-table-header>
+          <div>Template Name</div>
+          <div>Total Exercises</div>
+          <div>Actions</div>
+        </div>
+        <ul data-table-list>
+          <li>
+            <p>Push Day</p>
+            <p>
+              12 Exercises
+            </p>
+          </li>
+          <li>
+            <p>Push Day</p>
+            <p>
+              12 Exercises
+            </p>
+          </li>
+          <li>
+            <p>Push Day</p>
+            <p>
+              12 Exercises
+            </p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
